43da7b8cd0e9bef517a4abe68ea4c6c339eac8fb
Naya Olmer <nayaolmer@gmail.com>
2024-08-16 10:51:16 +0200

    enables basic login

---
+++ gorapass-frontend/src/components/CompletedHikes.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+
+
+
+const CompletedHikes = () => {
+  const [completedHikes, setCompletedHikes] = useState([])
+
+
+
+  useEffect(() => {
+    fetch("http://localhost:8000/gorapass/users/2/completed_hikes")
+      .then(response => response.json())
+      .then(json => setCompletedHikes(json))
+      .catch(error => console.error(error));
+  }, []);
+
+
+
+  return (
+    <div>
+      <h1>All Completed Hikes</h1>
+      <ul>
+         {completedHikes.map(hike =>
+          <li key={hike.id}>
+            {hike.hike_name}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+
+
+export default CompletedHikes
