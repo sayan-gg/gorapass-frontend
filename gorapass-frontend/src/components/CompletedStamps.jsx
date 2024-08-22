6050db25317710421d32b6ca7611fae209b36f6d
Naya Olmer <nayaolmer@gmail.com>
2024-08-22 16:15:50 +0200

    Fill empty CompletedStamps page with actual completed stamps

--- gorapass-frontend/src/components/CompletedStamps.jsx
+++ gorapass-frontend/src/components/CompletedStamps.jsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from 'react'
+import {Link} from "react-router-dom"
+
 
 const CompletedStamps = () => {
+  const [completedStamps, setCompletedStamps] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8000/gorapass/users/completed_stamps", {
+      credentials:'include'})
+      .then(response => response.json())
+      .then(json => setCompletedStamps(json))
+      .then(console.log(completedStamps[0]))
+      .catch(error => console.error(error));
+  }, []);
   return (
     <div>
       <h1>All Completed Stamps</h1>
+      <ul>
+        {completedStamps.map(stamp =>
+          <li key={stamp.id}>
+            <Link to={`/stamps/${stamp.id}`}>{stamp.stamp_name}</Link>
+            </li>
+        )}
+      </ul>
     </div>
   )
 }
