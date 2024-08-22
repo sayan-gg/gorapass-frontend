a5dee0e8ba624bf2b960501611fddf314e41ca98
Naya Olmer <nayaolmer@gmail.com>
2024-08-22 14:34:51 +0200

    Show open stamps for user

--- gorapass-frontend/src/components/OpenStamps.jsx
+++ gorapass-frontend/src/components/OpenStamps.jsx
@@ -1,10 +1,28 @@
-
+import { useState, useEffect } from 'react'
+import {Link} from "react-router-dom"
 
 const OpenStamps = () => {
+  const [openStamps, setOpenStamps] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8000/gorapass/users/open_stamps", {
+      credentials:'include'})
+      .then(response => response.json())
+      .then(json => setOpenStamps(json))
+      .then(console.log(openStamps[0]))
+      .catch(error => console.error(error));
+  }, []);
 
   return (
     <div>
       <h1>All Open Stamps</h1>
+      <ul>
+        {openStamps.map(stamp =>
+          <li key={stamp.id}>
+            <Link to={`/stamps/${stamp.id}`}>{stamp.stamp_name}</Link>
+            </li>
+        )}
+      </ul>
     </div>
   )
 }
